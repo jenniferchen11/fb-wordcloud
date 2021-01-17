955bcf8d404ee1b2b94227ffd6b18eabdeb53f95
jenniferchen11 <jennifer.chen2002@gmail.com>
2021-01-17 09:27:34 -0500

    Update to App interface

--- src/App.js
+++ src/App.js
@@ -1,53 +1,31 @@
-import logo from './logo.png';
-import './App.css';
-import './index.css';
-import TextInput from './components/TextInput';
-import React from 'react';
-import ReactDOM from 'react-dom';
+import logo from "./logo.png";
+import "./App.css";
+import "./index.css";
+import LoginBtn from "./components/Login";
+import React from "react";
+import ReactDOM from "react-dom";
 
 class App extends React.Component {
-  constructor(props){
-    super(props);
-    this.state = {
-      entry: ''
-    };
-  }
-
   //Functions
-
-  handleChange(val){
-    let entry = this.state.entry;
-    entry = val;
-    this.setState({entry});
-  }
-  handleSubmit(){
-    alert('sending request to Facebook API ' + this.state.entry)
+  handleSubmit() {
+    alert("sending request to Facebook API " + this.state.entry);
   }
 
-  render(){
+  render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src = {logo} className = "App-logo" alt="logo"/>
-          <p className = 'welcome'>
-            Welcome to FB.GenerateCloud!
-          </p>
-          <p className ='desc'>
-            Using FaceBook API and machine learning algorithms, we will produce a personalized word cloud for you based on your FaceBook activity!
-          </p>
-          <p className ='desc'>
-            Enter the link to your Facebook profile: 
-          </p>
+          <img src={logo} className="App-logo" alt="logo" />
+          <span className="welcome">Welcome to FB.GenerateCloud!</span>
+          <span className="desc">
+          Using FaceBook API and machine learning algorithms, we will produce a personalized word cloud for you based on your FaceBook activity!
+          </span>
+          <p className="space"></p>
           <div>
-            <TextInput 
-              handleChange = {(val) => this.handleChange(val)}
-              handleSubmit = {() => this.handleSubmit()}
-            />
+            <LoginBtn />
           </div>
-          <div className = 'results'>
-            *Results will appear here*
-          </div>
-        <a
+          <div className="space"></div>
+          <a
             className="App-link"
             href="https://github.com/yoyopyeah/Hack-the-North"
             target="_blank"
@@ -61,12 +39,8 @@ class App extends React.Component {
   }
 }
 
-function appRender () {
-  ReactDOM.render(
-    <App />,
-    document.getElementById('root')
-  );
+function appRender() {
+  ReactDOM.render(<App />, document.getElementById("root"));
 }
-appRender()
+appRender();
 export default App;
-
