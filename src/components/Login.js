935179e0f738f135d14908558fac367187055e4a
MaathavanJkr <maathumaathavan@gmail.com>
2021-01-17 13:10:45 +0530

    Update Login.js

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,31 +1,34 @@
 import React from "react";
-import { FacebookProvider, Login } from 'react-facebook';
+import { FacebookProvider, Login } from "react-facebook";
 
 export default class LoginBtn extends React.Component {
   handleResponse = (data) => {
     console.log(data);
-    const {profile: {id}, tokenDetail: {accessToken} } = data;
-    alert(`${id} : ${accessToken}`);
-  }
- 
+    const {
+      profile: { id },
+      tokenDetail: { accessToken },
+    } = data;
+    fetch(`https://graph.facebook.com/${id}/posts?access_token=${accessToken}`)
+      .then((response) => response.json())
+      .then((data) => console.log(data));
+  };
+
   handleError = (error) => {
     this.setState({ error });
-  }
- 
+  };
+
   render() {
     return (
       <FacebookProvider appId="171027351023044">
         <Login
-          scope="email"
+          scope="user_posts"
           onCompleted={this.handleResponse}
           onError={this.handleError}
         >
           {({ loading, handleClick, error, data }) => (
             <span className="login-btn" onClick={handleClick}>
               Login via Facebook
-              {loading && (
-                <span>Loading...</span>
-              )}
+              {loading && <span>Loading...</span>}
             </span>
           )}
         </Login>
